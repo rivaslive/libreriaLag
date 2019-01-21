737cedf979457492cf4d1719e7823ec3c54e81ca
Kevin Rivas <rl0185032015@unab.edu.sv>
2019-01-21 12:40:03 -0600

    venv

--- libreriaLag/settings.py
+++ libreriaLag/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.ventas',
+    'apps.articulos',
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'libreriaLag.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_libreria',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

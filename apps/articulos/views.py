b6ce1666ca9479e35498c94d31b676969853778c
Yonatan Ulises Segura <yonatan_yusc@yahoo.es>
2019-01-22 18:39:39 -0600

    Hice lo que me dijiste perro
    Hice lo del inventario, con 0 articulos, mayor que 1 pero menor que 10 y mayor que 10

--- apps/articulos/views.py
+++ apps/articulos/views.py
@@ -3,19 +3,33 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from apps.articulos.models import Articulo
 from apps.articulos.forms import ArticuloForm
+from django.db.models import Q
 
-#ver articulos
+
+# ver articulos
+
+
 def articulo(request):
-    query=Articulo.objects.filter(is_activate=1)
-    return render(request, 'productos/articulo.html', {'articulo':query})
+    query = Articulo.objects.filter(is_activate=1)
+    return render(request, 'productos/articulo.html', {'articulo': query})
 
 
-#crear articulos
+# crear articulos
 class CrearArticulo(CreateView):
     template_name = 'productos/productoModal.html'
     form_class = ArticuloForm
     success_url = reverse_lazy('articulo:articulo')
 
+# Mostrar los productos sin stock
+
 
 def inventario(request):
-    return render(request, 'productos/inventario.html')
+    # Articulos con estado activo, con stock mayor que 1 pero menores que 10
+    query = Articulo.objects.filter(
+        Q(is_activate=1) & Q(stock__gt=0) & Q(stock__lt=11))
+    # Articulos con 0 existencias
+    query_ar = Articulo.objects.filter(is_activate=1, stock=0)
+    # Todos los demás articulos mayores ue 10
+    query_are = Articulo.objects.filter(Q(is_activate=1) & Q(stock__gt=10))
+
+    return render(request, 'productos/inventario.html', {'inventario1': query, 'inventario': query_ar, 'inventariop': query_are})
---
+++ venv/Scripts/autopep8-script.py
@@ -0,0 +1,10 @@
+#!c:\Users\Yonatan-Segura\Documents\GitHub\libreriaLag\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'autopep8==1.4.3','console_scripts','autopep8'
+__requires__ = 'autopep8==1.4.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('autopep8==1.4.3', 'console_scripts', 'autopep8')()
+    )

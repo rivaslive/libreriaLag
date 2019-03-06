5b0657b4830f6611c9c8125da9139e7d10291103
Kevin Rivas <rl0185032015@unab.edu.sv>
2019-03-05 23:19:25 -0600

    pass mysql servidor

--- libreriaLag/settings.py
+++ libreriaLag/settings.py
@@ -79,8 +79,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_libreria',
         'USER': 'root',
-        #'PASSWORD': 'cementerio123',
-        'PASSWORD':'root',
+        'PASSWORD': 'cementerio123',
+        #'PASSWORD': 'Rangerblu7',
+        #'PASSWORD':'root',
         'HOST': 'localhost',
         'PORT': '',
     }

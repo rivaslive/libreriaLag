d56601e89f129afc625aff6d87bca8c316cac15b
Yonatan Ulises Segura <yonatan_yusc@yahoo.es>
2019-03-05 23:13:17 -0600

    fjdsj
    jksdjñfas

--- libreriaLag/settings.py
+++ libreriaLag/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_libreria',
         'USER': 'root',
-        'PASSWORD': 'cementerio123',
-        #'PASSWORD':'root',
+        #'PASSWORD': 'cementerio123',
+        'PASSWORD':'root',
         'HOST': 'localhost',
         'PORT': '',
     }
